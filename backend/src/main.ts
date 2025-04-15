a9427da832ee57bb9c26bad833a63e7efa53f795
alexdenuke <alexdenuke@gmail.com>
2025-04-16 02:36:59 +0300

    add console.log

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,5 +5,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   await app.listen(process.env.PORT ?? 5000);
+  console.log(
+    `✅ Server is running on http://0.0.0.0:${process.env.PORT ?? 5000}`,
+  );
 }
 bootstrap();
